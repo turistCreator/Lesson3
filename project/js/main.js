72c293e04b7a633f013373f598a9df56e90bce3a
turistCreator <student2229@mail.ru>
2022-04-21 17:26:40 +0300

    first commit

--- project/js/main.js
+++ project/js/main.js
@@ -1,20 +1,18 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-let getRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState === 4){
-                if(xhr.status !== 200){
-                    reject('Error');
-                } else {
-                    resolve(xhr.responseText);
-                }
+let getRequest = (url, cb) => { // не fetch
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+                console.log('Error');
+            } else {
+                cb(xhr.responseText);
             }
-        };
-        xhr.send();
-    })
+        }
+    };
+    xhr.send();
 };
 
 class ProductList {
